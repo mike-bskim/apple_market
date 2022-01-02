5ca3426c57837900a64174d30d0bbd9c9ee8ac67
mike.bskim <mike.bskim@gmail.com>
2022-01-02 22:03:00 +0100

    TextButton with cupertino icon

--- lib/src/pages/start/address_page.dart
+++ lib/src/pages/start/address_page.dart
@@ -1,7 +1,14 @@
+import 'package:apple_market/src/utils/logger.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddressPage extends StatelessWidget {
   const AddressPage({Key? key}) : super(key: key);
+
+  void onButtonClick(){
+    logger.d('address_page >> on Text Button Clicked !!!');
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +25,22 @@ class AddressPage extends StatelessWidget {
               hintText: '도로명으로 검색',
               hintStyle: TextStyle(color: Theme.of(context).hintColor),
             ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextButton.icon(
+                  icon: const Icon(CupertinoIcons.compass, color: Colors.white, size: 20,),
+                  onPressed: onButtonClick,
+                  label: Text('현재위치 찾기',
+                    style: Theme.of(context).textTheme.button,
+                  ),
+                  style: TextButton.styleFrom(backgroundColor: Theme.of(context).primaryColor),
+                ),
+              ),
+            ],
           ),
         ],
       ),

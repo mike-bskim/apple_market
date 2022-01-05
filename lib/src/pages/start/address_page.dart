6a309b1a3f2d7bb793e58c146b0aac8b10d8c944
mike.bskim <mike.bskim@gmail.com>
2022-01-05 16:36:57 +0100

    show address from coordinate

--- lib/src/pages/start/address_page.dart
+++ lib/src/pages/start/address_page.dart
@@ -1,10 +1,12 @@
 import 'package:apple_market/src/constants/common_size.dart';
 import 'package:apple_market/src/model/address_model.dart';
+import 'package:apple_market/src/model/address_modelXY.dart';
 import 'package:apple_market/src/utils/logger.dart';
 import 'package:apple_market/src/pages/start/address_service.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class AddressPage extends StatefulWidget {
   const AddressPage({Key? key}) : super(key: key);
@@ -17,17 +19,8 @@ class _AddressPageState extends State<AddressPage> {
   final TextEditingController _addressController = TextEditingController();
 
   AddressModel? _addressModel;
-
-  void onClickSearchAddress() async {
-    final text = _addressController.text;
-    logger.d('text:' + text);
-    if (text.isNotEmpty) {
-      FocusScope.of(context).unfocus();
-      _addressModel = await AddressService().searchAddressByStr(text);
-      setState(() {});
-    }
-    logger.d('address_page >> on Text Button Clicked !!!');
-  }
+  final List<AddressModelXY> _addressModelXYs = [];
+  bool _isGettingLocation = false;
 
   @override
   Widget build(BuildContext context) {
@@ -57,16 +50,18 @@ class _AddressPageState extends State<AddressPage> {
             ),
           ),
           TextButton.icon(
-            onPressed: onClickSearchAddress,
-            icon: const Icon(
-              CupertinoIcons.compass,
-              color: Colors.white,
-              size: 20,
-            ),
             label: Text(
-              '현재위치 찾기',
+              _isGettingLocation ? '위치 찾는중 ~~' : '현재위치 찾기',
               style: Theme.of(context).textTheme.button,
             ),
+            onPressed: myLocation,
+            icon: _isGettingLocation
+                ? const CircularProgressIndicator()
+                : const Icon(
+                    CupertinoIcons.compass,
+                    color: Colors.white,
+                    size: 20,
+                  ),
           ),
           Expanded(
             child: ListView.builder(
@@ -96,5 +91,58 @@ class _AddressPageState extends State<AddressPage> {
         ],
       ),
     );
+  }
+
+  void onClickSearchAddress() async {
+    final text = _addressController.text;
+    logger.d('text:' + text);
+    if (text.isNotEmpty) {
+      FocusScope.of(context).unfocus();
+      _addressModel = await AddressService().searchAddressByStr(text);
+      setState(() {});
+    }
+    logger.d('address_page >> on Text Button Clicked !!!');
+  }
+
+  void myLocation() async {
+
+    _addressModel = null;
+
+    setState(() {
+      _isGettingLocation = true;
+    });
+    Location location = Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+    logger.d('_locationData: ${_locationData.toString()}');
+    List<AddressModelXY> _addressModelXY = await AddressService().findAddressByCoordinate(
+        // log: _locationData.longitude!, lat: _locationData.latitude!);
+        log: 126.71447360681148,
+        lat: 37.36341055367434);
+    _addressModelXYs.addAll(_addressModelXY);
+
+    setState(() {
+      _isGettingLocation = false;
+    });
   }
 }
--- lib/src/pages/start/address_service.dart
+++ lib/src/pages/start/address_service.dart
@@ -1,5 +1,6 @@
 import 'package:apple_market/src/constants/keys.dart';
 import 'package:apple_market/src/model/address_model.dart';
+import 'package:apple_market/src/model/address_modelXY.dart';
 import 'package:dio/dio.dart';
 import 'package:apple_market/src/utils/logger.dart';
 // import 'package:flutter/cupertino.dart';
@@ -28,12 +29,59 @@ class AddressService {
       .catchError((e){
         logger.e(e.message);
       });
-    logger.d('resp: [$resp]');
-    logger.d('resp.data is Map: [${resp.data is Map}]');
 
     AddressModel addressModel = AddressModel.fromJson(resp.data['response']);
-    logger.d('addressService: [$addressModel]');
 
     return addressModel;
+  }
+
+  Future<List<AddressModelXY>> findAddressByCoordinate({required double log, required double lat}) async {
+
+    List<Map<String, dynamic>> formData = <Map<String, dynamic>>[];
+
+    for (var i=0; i<5; i++) {
+      int x;
+      int y;
+      if (i==1){x = 0; y=1;}
+      else if (i==2){x = 1; y=0;}
+      else if (i==3){x = 0; y=-1;}
+      else if (i==4){x = -1; y=0;}
+      else {x = 0; y=0;}
+
+
+      formData.add({
+        'key':vWorldKey,
+        'service':'address',
+        'request':'getAddress',
+        'type':'BOTH',
+        'point':'${log+0.01*x},${lat+0.01*y}',
+      });
+    }
+    // final formData = {
+    //   'key':vWorldKey,
+    //   'service':'address',
+    //   'request':'getAddress',
+    //   'type':'BOTH',
+    //   'point':'$log,$lat',
+    // };
+
+    List<AddressModelXY> addressModelXYs = [];
+
+    for(var data in formData) {
+      var resp = await Dio().get('http://api.vworld.kr/req/address', queryParameters: data)
+          .catchError((e){
+        logger.e(e.message);
+      });
+      // logger.d('resp[response]: ${resp.data['response']}');
+      // logger.d('resp[status]: ${resp.data['response']['status']}');
+
+      AddressModelXY addressModelXY = AddressModelXY.fromJson(resp.data['response']);
+      if(resp.data['response']['status'] == 'OK'){
+        addressModelXYs.add(addressModelXY);
+      }
+    }
+    logger.d('길이: ${addressModelXYs.length}');
+
+    return addressModelXYs;
   }
 }

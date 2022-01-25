b96f7ee91a8b28d9756e2d4447dd9cc83d4b8893
mike.bskim <mike.bskim@gmail.com>
2022-01-25 12:25:33 +0100

    detail body layout

--- lib/src/screens/item_detail_screen.dart
+++ lib/src/screens/item_detail_screen.dart
@@ -15,56 +15,6 @@ class ItemDetailScreen extends StatefulWidget {
   _ItemDetailScreenState createState() => _ItemDetailScreenState();
 }
 
-// class _ItemDetailScreenState extends State<ItemDetailScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     logger.d('item detail screen >> build >>> [${widget.itemKey}]');
-//
-//     return FutureBuilder<ItemModel>(
-//       future: ItemService().getItem(widget.itemKey),
-//       builder: (context, snapshot) {
-//         if(snapshot.hasData){
-//           return LayoutBuilder(
-//             builder: (context, constraints) {
-//               Size _size = MediaQuery.of(context).size;
-//
-//               return Scaffold(
-//                 body: CustomScrollView(
-//                   slivers: [
-//                     SliverAppBar(
-//                       expandedHeight: _size.width,
-//                       pinned: true,
-//                       flexibleSpace: const FlexibleSpaceBar(
-//                         title: Text(
-//                           'testing',
-//                           style: TextStyle(color: Colors.black87),
-//                         ),
-//                         background: FlutterLogo(),
-//                       ),
-//                     ),
-//                     SliverToBoxAdapter(
-//                       child: Container(
-//                         height: _size.height * 2,
-//                         color: Colors.blue,
-//                         child: Center(child: Text('Item key is ${widget.itemKey}')),
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               );
-//             },
-//           );
-//         } else {
-//           return Container(
-//             child: const Center(child: Text("No data")),
-//           );
-//         }
-//
-//       }
-//     );
-//   }
-// }
-
 class _ItemDetailScreenState extends State<ItemDetailScreen> {
   final PageController _pageController = PageController();
   final ScrollController _scrollController = ScrollController();
@@ -72,6 +22,18 @@ class _ItemDetailScreenState extends State<ItemDetailScreen> {
   bool isAppbarCollapsed = false;
   Size? _size;
   num? _statusBarHeight;
+
+  final Widget _textGap = const SizedBox(height: padding_16);
+
+  Widget _divider(double _height) {
+    return Divider(
+      height: _height, //padding_16 * 2 + 1,
+      thickness: 2,
+      color: Colors.grey[300]!,
+      // indent: padding_08,
+      // endIndent: padding_08,
+    );
+  }
 
   @override
   void initState() {
@@ -118,7 +80,9 @@ class _ItemDetailScreenState extends State<ItemDetailScreen> {
               return Stack(
                 fit: StackFit.expand,
                 children: [
+                  // 메인 정보를 표시하는 영역
                   Scaffold(
+                    // 화면 하단을 네비로 표시함
                     bottomNavigationBar: SafeArea(
                       top: false,
                       bottom: true,
@@ -136,9 +100,9 @@ class _ItemDetailScreenState extends State<ItemDetailScreen> {
                             children: [
                               IconButton(
                                 onPressed: () {},
-                                icon: Icon(Icons.favorite_border),
+                                icon: const Icon(Icons.favorite_border),
                               ),
-                              VerticalDivider(
+                              const VerticalDivider(
                                 thickness: 1,
                                 width: padding_08 * 2 + 1,
                                 indent: padding_08,
@@ -148,64 +112,94 @@ class _ItemDetailScreenState extends State<ItemDetailScreen> {
                                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text('4000원', style: Theme.of(context).textTheme.bodyText1,),
-                                  Text('가격제안불가', style: Theme.of(context).textTheme.bodyText2,),
+                                  Text(
+                                    '4000원',
+                                    style: Theme.of(context).textTheme.bodyText1,
+                                  ),
+                                  Text(
+                                    '가격제안불가',
+                                    style: Theme.of(context).textTheme.bodyText2,
+                                  ),
                                 ],
                               ),
                               Expanded(child: Container()),
                               TextButton(
                                 onPressed: () {},
-                                child: Text('채팅으로 거래하기'),
+                                child: const Text('채팅으로 거래하기'),
                               ),
                             ],
                           ),
                         ),
                       ),
                     ),
+                    // 메인정보를 표시
                     body: CustomScrollView(
                       controller: _scrollController,
                       slivers: [
-                        SliverAppBar(
-                          expandedHeight: _size!.width,
-                          pinned: true,
-                          flexibleSpace: FlexibleSpaceBar(
-                            centerTitle: true,
-                            title: SizedBox(
-                              child: SmoothPageIndicator(
-                                  controller: _pageController,
-                                  // PageController
-                                  count: itemModel.imageDownloadUrls.length,
-                                  effect: WormEffect(
-                                    activeDotColor: Theme.of(context).primaryColor,
-                                    dotColor: Theme.of(context).colorScheme.background,
-                                    radius: 3,
-                                    dotHeight: 6,
-                                    dotWidth: 6,
-                                  ),
-                                  // your preferred effect
-                                  onDotClicked: (index) {}),
-                            ),
-                            background: PageView.builder(
-                              controller: _pageController,
-                              allowImplicitScrolling: true,
-                              itemBuilder: (BuildContext context, int index) {
-                                return ExtendedImage.network(
-                                  itemModel.imageDownloadUrls[index],
-                                  fit: BoxFit.cover,
-                                  scale: 0.1,
-                                );
-                              },
-                              itemCount: itemModel.imageDownloadUrls.length,
-                            ),
-                          ),
-                        ),
-                        SliverList(
+                        // 업로드한 사진 정보를 표시하는 영역
+                        _imageAppBar(itemModel),
+                        // 상세 텍스트 정보를 표시하는 영역
+                        SliverPadding(
+                          padding: const EdgeInsets.all(padding_16),
+                          sliver: SliverList(
                             delegate: SliverChildListDelegate([
-                          _userSection(),
-                        ]))
+                              _userSection(),
+                              _divider(padding_16 * 2 + 1),
+                              Text(
+                                '카즈미 캠핑키진 툴',
+                                style: Theme.of(context).textTheme.headline6,
+                              ),
+                              _textGap,
+                              Row(
+                                children: [
+                                  Text(
+                                    '스포츠/레저',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyText2!
+                                        .copyWith(decoration: TextDecoration.underline),
+                                  ),
+                                  Text(
+                                    ' · 2분전',
+                                    style: Theme.of(context).textTheme.bodyText2,
+                                  ),
+                                ],
+                              ),
+                              _textGap,
+                              Text(
+                                '한번사용했어요\n'
+                                '흠집이나 하자없고 깨긋합니다',
+                                style: Theme.of(context).textTheme.bodyText1,
+                              ),
+                              _textGap,
+                              Text(
+                                '조회 33',
+                                style: Theme.of(context).textTheme.caption,
+                              ),
+                              _textGap,
+                              _divider(2),
+                              MaterialButton(
+                                padding: EdgeInsets.zero,
+                                onPressed: () {},
+                                child: const Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    '이 게시글 신고하기',
+                                    // style: Theme.of(context)
+                                    //     .textTheme
+                                    //     .button!
+                                    //     .copyWith(color: Colors.black87),
+                                  ),
+                                ),
+                              ),
+                              _divider(2),
+                            ]),
+                          ),
+                        )
                       ],
                     ),
                   ),
+                  // 앱바 영역에 그라데이션 표현 추가
                   Positioned(
                     left: 0,
                     right: 0,
@@ -228,6 +222,7 @@ class _ItemDetailScreenState extends State<ItemDetailScreen> {
                       ),
                     ),
                   ),
+                  // 화면 스크롤업 하면 앱바를 힌색으로 변경.
                   Positioned(
                     left: 0,
                     right: 0,
@@ -255,86 +250,124 @@ class _ItemDetailScreenState extends State<ItemDetailScreen> {
     );
   }
 
-  Widget _userSection() {
-    return Padding(
-      padding: const EdgeInsets.all(padding_08),
-      child: Row(
-        children: [
-          ExtendedImage.network(
-            'https://picsum.photos/50',
-            fit: BoxFit.cover,
-            width: _size!.width / 10,
-            height: _size!.width / 10,
-            shape: BoxShape.circle,
-          ),
-          SizedBox(width: padding_16),
-          SizedBox(
-            height: _size!.width / 10,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '무무',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                Text(
-                  '원효로',
-                  style: Theme.of(context).textTheme.bodyText2,
-                ),
-              ],
-            ),
-          ),
-          Expanded(child: Container()),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Row(
-                children: [
-                  SizedBox(
-                    width: 40,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        FittedBox(
-                          child: Text(
-                            '37.3',
-                            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent),
-                          ),
-                        ),
-                        SizedBox(height: 6),
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(1),
-                          child: LinearProgressIndicator(
-                            color: Colors.blueAccent,
-                            value: 0.365,
-                            minHeight: 3,
-                            backgroundColor: Colors.grey[200],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  SizedBox(width: 8),
-                  const ImageIcon(
-                    ExtendedAssetImageProvider('assets/imgs/happiness.png'),
-                    color: Colors.blue,
-                  ),
-                ],
+  SliverAppBar _imageAppBar(ItemModel itemModel) {
+    return SliverAppBar(
+      expandedHeight: _size!.width,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        // 인디케이터 표시
+        title: SizedBox(
+          child: SmoothPageIndicator(
+              controller: _pageController,
+              // PageController
+              count: itemModel.imageDownloadUrls.length,
+              effect: const WormEffect(
+                activeDotColor: Colors.white,
+                //Theme.of(context).primaryColor,
+                dotColor: Colors.white24,
+                //Theme.of(context).colorScheme.background,
+                radius: 3,
+                dotHeight: 6,
+                dotWidth: 6,
               ),
-              SizedBox(height: padding_08),
+              // your preferred effect
+              onDotClicked: (index) {}),
+        ),
+        // 이미지 표시
+        background: PageView.builder(
+          controller: _pageController,
+          allowImplicitScrolling: true,
+          itemBuilder: (BuildContext context, int index) {
+            return ExtendedImage.network(
+              itemModel.imageDownloadUrls[index],
+              fit: BoxFit.cover,
+              scale: 0.1,
+            );
+          },
+          itemCount: itemModel.imageDownloadUrls.length,
+        ),
+      ),
+    );
+  }
+
+  Widget _userSection() {
+    return Row(
+      children: [
+        ExtendedImage.network(
+          'https://picsum.photos/50',
+          fit: BoxFit.cover,
+          width: _size!.width / 10,
+          height: _size!.width / 10,
+          shape: BoxShape.circle,
+        ),
+        const SizedBox(width: padding_16),
+        SizedBox(
+          height: _size!.width / 10,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
               Text(
-                '매너온도',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyText2!
-                    .copyWith(decoration: TextDecoration.underline),
+                '무무',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              Text(
+                '원효로',
+                style: Theme.of(context).textTheme.bodyText2,
               ),
             ],
           ),
-          // Text('aaa'),
-        ],
-      ),
+        ),
+        Expanded(child: Container()),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  width: 42,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const FittedBox(
+                        child: Text(
+                          '37.3 °C',
+                          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent),
+                        ),
+                      ),
+                      const SizedBox(height: 6),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(1),
+                        child: LinearProgressIndicator(
+                          color: Colors.blueAccent,
+                          value: 0.365,
+                          minHeight: 3,
+                          backgroundColor: Colors.grey[200],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 6),
+                const ImageIcon(
+                  ExtendedAssetImageProvider('assets/imgs/happiness.png'),
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+            const SizedBox(height: padding_08),
+            Text(
+              '매너온도',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText2!
+                  .copyWith(decoration: TextDecoration.underline),
+            ),
+          ],
+        ),
+        // Text('aaa'),
+      ],
     );
   }
 }

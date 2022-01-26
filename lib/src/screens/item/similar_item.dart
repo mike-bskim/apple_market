3ae090f773bb4525598f142740da9478d80af841
mike.bskim <mike.bskim@gmail.com>
2022-01-27 00:17:35 +0100

    how to stack the item detail pages

--- lib/src/screens/item/similar_item.dart
+++ lib/src/screens/item/similar_item.dart
@@ -1,5 +1,6 @@
 import 'package:apple_market/src/constants/common_size.dart';
 import 'package:apple_market/src/model/item_model.dart';
+import 'package:apple_market/src/screens/item_detail_screen.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 
@@ -9,30 +10,37 @@ class SimilarItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        AspectRatio(
-          aspectRatio: 5/4,
-          child: ExtendedImage.network(
-            _itemModel.imageDownloadUrls[0],
-            fit: BoxFit.cover,
-            borderRadius: BorderRadius.circular(8),
-            shape: BoxShape.rectangle,
+    return InkWell(
+      onTap: (){
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+          return ItemDetailScreen(_itemModel.itemKey);
+        }));
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          AspectRatio(
+            aspectRatio: 5/4,
+            child: ExtendedImage.network(
+              _itemModel.imageDownloadUrls[0],
+              fit: BoxFit.cover,
+              borderRadius: BorderRadius.circular(8),
+              shape: BoxShape.rectangle,
+            ),
           ),
-        ),
-        Text(
-          _itemModel.title,
-          overflow: TextOverflow.ellipsis,
-          maxLines: 1,
-          style: Theme.of(context).textTheme.subtitle1,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: padding_08),
-          child: Text('${_itemModel.price.toString()}원',style: Theme.of(context).textTheme.subtitle2,),
-        ),
-      ],
+          Text(
+            _itemModel.title,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: padding_08),
+            child: Text('${_itemModel.price.toString()}원',style: Theme.of(context).textTheme.subtitle2,),
+          ),
+        ],
+      ),
     );
   }
 }
